d6a8a519e7fe0bdb4ee4725301b24386ba789e47
Kots Olesya <kots.olesya@gmail.com>
2016-12-17 17:23:43 +0200

    test SignUp end SignIn, gravatar

--- app/models/task.rb
+++ app/models/task.rb
@@ -7,14 +7,4 @@ class Task < ApplicationRecord
   scope :filtered, ->(type) { send(type) if type }
   scope :done, -> { where(done: true) }
   scope :open, -> { where(done: false) }
-
-  def self.order_task(sort_order)
-    if sort_order == 'newest' || sort_order.blank?
-      order(created_at: :desc)
-    elsif sort_order == 'title'
-      order(title: :asc)
-    else
-      order(created_at: :asc)
-    end
-  end
 end
